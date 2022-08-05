402edc7217adcb98d322dc9e8b300266d3e64aa1
李伟兴 <2710156303@qq.com>
2022-08-05 11:03:30 +0800

    feat:first commit

---
+++ .eslintrc.js
@@ -0,0 +1,32 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:vue/vue3-essential", //这里是个坑
+  ],
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: ["vue", "@typescript-eslint"],
+  rules: {
+    "vue/multi-word-component-names": "off",
+    "import/no-unresolved": "off",
+    "import/extensions": "off",
+    "import/no-absolute-path": "off",
+    "import/no-extraneous-dependencies": "off",
+    "vue/no-multiple-template-root": "off",
+    "no-param-reassign": [
+      "error",
+      {
+        props: true,
+        ignorePropertyModificationsFor: ["state", "config"],
+      },
+    ],
+  },
+  settings: {},
+};
---
+++ src/views/Login/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div> 这是登录的页面 </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
